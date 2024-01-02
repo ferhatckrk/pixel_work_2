295cb575053610c02cf284a64ecc2dace198658f
Ferhat Çıkrık <ferhatckrk@outlook.com>
2024-01-02 12:11:21 +0300

    boyutlar çüçültüldü

--- lib/home_view4.dart
+++ lib/home_view4.dart
@@ -1,14 +1,34 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_randomcolor/flutter_randomcolor.dart';
+import 'package:grid_work_1/core/extension/extensions.dart';
 
-class BlocType {
-  BlockTypes blockColor;
+class Bloc {
+  Color blockColor;
   int index;
-  BlocType({
-    required this.blockColor,
-    required this.index,
-  });
+  double? top;
+  double? left;
+  Bloc({required this.blockColor, required this.index, this.top, this.left});
+
+  Bloc copyWith({
+    Color? blockColor,
+    int? index,
+    double? top,
+    double? left,
+  }) {
+    return Bloc(
+      blockColor: blockColor ?? this.blockColor,
+      index: index ?? this.index,
+      top: top ?? this.top,
+      left: left ?? this.left,
+    );
+  }
+}
+
+class Data {
+  static List<Bloc> blocks = [];
 }
 
 class HomeView4 extends StatelessWidget {
@@ -31,33 +51,37 @@ class HomeView4 extends StatelessWidget {
   }
 }
 
-const int redCount = 100000;
+const int redCount = 10000;
 
-const int allCount = 100000;
-const int crossAxisCount = 250;
+const int allCount = 10000;
+const int crossAxisCount = 100;
 
-enum BlockTypes { red, yellow }
+//enum Color { red, yellow }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({super.key})
-      : blocks = List<BlockTypes>.generate(allCount, (index) {
-          return BlockTypes.red;
-        });
+  final Random random = Random();
+  var options = Options(format: Format.hex, colorType: ColorType.green);
+  MyHomePage({super.key}) {
+    Data.blocks = List<Bloc>.generate(allCount, (index) {
+      String color = RandomColor.getColor(options);
 
-  final List<BlockTypes> blocks;
+      return Bloc(blockColor: HexColor.fromHex(color), index: index);
+    });
+  }
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late int columnsCount;
-  late double blocSize;
+  int columnsCount = 0;
+  double blocSize = 0;
 
   late int clickedIndex;
   late Offset clickOffset;
+  late int onMoveIndex = 0;
   bool hasSizes = false;
-  late List<BlockTypes> blocks;
+  //late List<BlocType> blocks;
   final ScrollController scrollController = ScrollController();
 
   ValueNotifier<Offset> glassPosition = ValueNotifier(Offset.zero);
@@ -65,7 +89,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     WidgetsBinding.instance.addPostFrameCallback(_afterLayout);
-    blocks = widget.blocks;
+    // blocks = Data.blocks;
     super.initState();
   }
 
@@ -90,8 +114,24 @@ class _MyHomePageState extends State<MyHomePage> {
     clickedIndex = tapedColumn * crossAxisCount + tapedRow;
 
     setState(() {
-      blocks[clickedIndex] = BlockTypes.yellow;
+      Data.blocks[clickedIndex].blockColor = Colors.yellow;
     });
+  }
+
+  int onMove(Offset dimension) {
+    if (dimension.dx != 0 && dimension.dy != 0) {
+      final dx = dimension.dx;
+      final dy = dimension.dy + scrollController.offset;
+      final tapedRow = (dx / blocSize).floor();
+      final tapedColumn = (dy / blocSize).floor();
+      onMoveIndex = tapedColumn * crossAxisCount + tapedRow;
+      if (onMoveIndex >= 0 && onMoveIndex <= 10000) {
+        return onMoveIndex;
+      }
+    }
+    return 0;
+
+    // return Data.blocks[onMoveIndex];
   }
 
   bool isOpen = false;
@@ -105,17 +145,12 @@ class _MyHomePageState extends State<MyHomePage> {
         onTertiaryLongPressDown: (details) {
           glassPosition.value = details.localPosition;
           isOpen = true;
-          setState(() {});
         },
         onPanUpdate: (DragUpdateDetails details) {
           glassPosition.value = details.localPosition;
-
-          setState(() {});
         },
         onPanEnd: (details) {
           glassPosition.value = Offset.zero;
-
-          setState(() {});
         },
         onTapDown: onTapDown,
         onTap: onTap,
@@ -129,25 +164,32 @@ class _MyHomePageState extends State<MyHomePage> {
                   columnsCount * blocSize,
                 ),
                 painter: CustomGridView(
-                  blocs: widget.blocks,
+                  //  blocs: widget.blocks,
                   columnsCount: columnsCount,
                   blocSize: blocSize,
                 ),
               ),
             ),
             Positioned(
-              left: value.dx - 50,
-              top: value.dy - 50,
+              left: value.dx - 100,
+              top: value.dy - 100,
               child: Container(
-                decoration: const BoxDecoration(color: Colors.red, boxShadow: [
-                  BoxShadow(
-                    blurRadius: 10,
-                    color: Colors.black,
-                    offset: Offset(0, 0),
-                  )
-                ]),
-                height: 50,
-                width: 50,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    color: Data.blocks[onMove(value)].blockColor,
+                    boxShadow: const [
+                      BoxShadow(
+                        blurRadius: 10,
+                        color: Colors.black,
+                        offset: Offset(0, 0),
+                      )
+                    ]),
+                height: 100,
+                width: 100,
+                child: Text(
+                  "x: ${value.dx.toString().split('.').first}\ny: ${value.dy.toString().split('.').first}\n clr:${Data.blocks[onMove(value)].blockColor.value}\n index: ${onMove(value)}",
+                  style: const TextStyle(fontSize: 13),
+                ),
               ) /* Stack(
                 alignment: Alignment.center,
                 children: [
@@ -176,39 +218,47 @@ class _MyHomePageState extends State<MyHomePage> {
 class CustomGridView extends CustomPainter {
   final double gap = 1;
   final Paint painter = Paint()
-    ..strokeWidth = 10
+    ..strokeWidth = 5
     ..style = PaintingStyle.fill;
 
   final int columnsCount;
   final double blocSize;
-  final List<BlockTypes> blocs;
+  //final List<BlocType> blocs;
 
-  CustomGridView(
-      {required this.columnsCount,
-      required this.blocSize,
-      required this.blocs});
+  CustomGridView({
+    required this.columnsCount,
+    required this.blocSize,
+    /* required this.blocs */
+  });
 
   @override
   void paint(Canvas canvas, Size size) {
-    blocs.asMap().forEach((index, bloc) {
-      setColor(bloc);
+    Data.blocks.asMap().forEach((index, bloc) async {
+      setColor(bloc.blockColor);
 
-      Paint paint2 = Paint()
+/*       Paint paint2 = Paint()
         ..color = Colors.black
         ..strokeCap = StrokeCap.butt
         ..strokeWidth = 2 // getPixel(1)
-        ..style = PaintingStyle.fill;
+        ..style = PaintingStyle.fill; */
 
+      var topPosition = getTop(index);
+      var leftPosition = getLeft(index);
+      Data.blocks[index] = Data.blocks[index].copyWith(
+        top: topPosition,
+        left: leftPosition,
+      );
       canvas.drawRRect(
           RRect.fromRectAndRadius(
               Rect.fromLTWH(
-                getLeft(index),
-                getTop(index),
-                1.6 - 0.2,
-                1.6 - 0.2,
+                leftPosition, topPosition, blocSize - gap, blocSize - gap,
+                // 1.6 - 0.2,
+                //  1.6 - 0.2,
               ),
-              const Radius.circular(0)),
+              const Radius.circular(1)),
           painter);
+
+      await Future.delayed(Durations.medium1);
     });
   }
 
@@ -225,15 +275,16 @@ class CustomGridView extends CustomPainter {
   @override
   bool shouldRebuildSemantics(CustomGridView oldDelegate) => true;
 
-  void setColor(BlockTypes bloc) {
-    switch (bloc) {
-      case BlockTypes.red:
+  void setColor(Color blocColor) {
+    painter.color = blocColor;
+    /*   switch (bloc) {
+      case Color.red:
         painter.color = Colors.red;
         break;
 
-      case BlockTypes.yellow:
+      case Color.yellow:
         painter.color = Colors.yellow;
         break;
-    }
+    } */
   }
 }
